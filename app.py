5a75a71fc8b730d46428b5de90d88a98ec1458ab
deboralbupaiva <166133201+deboralbupaiva@users.noreply.github.com>
2025-02-17 10:11:28 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 print("""𝕊𝕒𝕣𝕓𝕠𝕞""")
+
 print("1 - Cadastrar restaurante")
-print("2 - Listar restaurante")
+print("2 - Listar restaurante ")
 print("3 - Ativar restaurante")
 print("4 - Sair")
 
-opcao_escolhida = input('Escolha uma opção: ')
-print(f"Você escolheu a opção: {opcao_escolhida}")
+opcao_escolhida = int(input('Escolha uma opção: '))
 
+if opcao_escolhida == 1:
+      print('Cadastrar restaurante')
+elif opcao_escolhida == 2:
+      print('Listar restaurante')
+elif opcao_escolhida == 3:
+      print('Ativar restaurante')
+else:
+      print('Encerrando programa')
